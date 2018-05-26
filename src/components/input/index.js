86ee753bd367f29e5911cd8f7beaff849a1a714d
Emmanuel Addo Narh <eanarh@gmail.com>
2018-05-26 16:30:31 -0500

    feat: updated styles for input

--- src/components/input/index.js
+++ src/components/input/index.js
@@ -3,69 +3,79 @@ import PropTypes from 'prop-types';
 import styled, { css } from 'styled-components';
 
 const StyledInput = styled.input`
+  background-color: var(--hiq-input-background-color, white);
+  border-radius: var(--hiq-input-border-radius, var(--hiq-border-radius, 0.2rem));
+  border: var(--hiq-input-border-width, 1px) solid var(--hiq-input-border-color, var(--hiq-gray-lighter, hsl(220, 10%, 90%)));
+  color: var(--hiq-input-text-color, var(--hiq-text-color, var(--hiq-gray-darker, hsl(220, 10%, 10%))));
   display: block;
-  width: 100%;
+  font-family: inherit;
+  font-size: inherit;
+  font-weight: var(--hiq-font-weight-base, 400);
   height: var(--hiq-input-height, 2.5rem);
+  line-height: inherit;
+  margin: 0;
   padding: var(--hiq-input-padding-vertical, 0) var(--hiq-input-padding-horizontal, 0.75rem);
-
-  ${props => props.isValid && css`
-    border-color: var(--hiq-color-success);
-  `}
+  transition: border-color var(--hiq-speed, 0.1s) var(--hiq-easing, ease-out), color var(--hiq-speed, 0.1s) var(--hiq-easing, ease-out), background-color var(--hiq-speed, 0.1s) var(--hiq-easing, ease-out);
+  width: 100%;
+  
 
   &[type='search'] {
     outline-offset: -2px;
     -webkit-appearance: none;
   }
 
-  margin: 0;
-  border: var(--hiq-input-border-width, 1px) solid var(--hiq-input-border-color, var(--hiq-gray-lighter, hsl(220, 10%, 90%)));
-  border-radius: var(--hiq-input-border-radius, var(--hiq-border-radius, 0.2rem));
-  background-color: var(--hiq-input-background-color, white);
-  font-family: inherit;
-  font-size: inherit;
-  font-weight: var(--hiq-font-weight-base, 400);
-  line-height: inherit;
-  color: var(--hiq-input-text-color, var(--hiq-text-color, var(--hiq-gray-darker, hsl(220, 10%, 10%))));
-  transition: border-color var(--hiq-speed, 0.1s) var(--hiq-easing, ease-out), color var(--hiq-speed, 0.1s) var(--hiq-easing, ease-out), background-color var(--hiq-speed, 0.1s) var(--hiq-easing, ease-out);
   &::placeholder {
     color: var(--hiq-input-placeholder-color, var(--hiq-gray, hsl(220, 10%, 40%)));
   }
+
   &:hover {
     border-color: var(--hiq-input-hover-border-color, var(--hiq-input-border-color, var(--hiq-gray-lighter, hsl(220, 10%, 90%))));
     background-color: var(--hiq-input-hover-background-color, var(--hiq-input-background-color, white));
     color: var(--hiq-input-hover-text-color, var(--hiq-input-text-color, var(--hiq-text-color, var(--hiq-gray-darker, hsl(220, 10%, 10%)))));
+    
     &::placeholder {
       color: var(--hiq-input-hover-placeholder-color, var(--hiq-input-placeholder-color, var(--hiq-gray, hsl(220, 10%, 40%))));
     }
   }
+
   &:focus {
-    border-color: var(--hiq-input-focus-border-color, var(--hiq-color-primary, hsl(210, 100%, 50%)));
     background-color: var(--hiq-input-focus-background-color, var(--hiq-input-background-color, white));
+    border-color: var(--hiq-input-focus-border-color, var(--hiq-color-primary, hsl(210, 100%, 50%)));
     box-shadow: 0 0 0 var(--hiq-outline-width, 0.2rem) var(--hiq-outline-color, hsl(210, 100%, 85%));
     color: var(--hiq-input-focus-text-color, var(--hiq-input-text-color, var(--hiq-text-color, var(--hiq-gray-darker, hsl(220, 10%, 10%)))));
+    
     &::placeholder {
       color: var(--hiq-input-focus-placeholder-color, var(--hiq-input-placeholder-color, var(--hiq-gray, hsl(220, 10%, 40%))));
     }
   }
+
   &:disabled,
   &[aria-disabled],
   &[readonly] {
     border-color: var(--hiq-disabled-border-color, var(--hiq-gray-lighter, hsl(220, 10%, 90%)));
     background-color: var(--hiq-disabled-background-color, var(--hiq-gray-lightest, hsl(220, 10%, 95%)));
     color: var(--hiq-disabled-text-color, var(--hiq-gray-light, hsl(220, 10%, 60%)));
+    
     &::placeholder {
       color: var(--hiq-disabled-text-color, var(--hiq-gray-light, hsl(220, 10%, 60%)));
     }
   }
+
   &:disabled {
     cursor: not-allowed;
   }
+
   &:required:invalid:not(:focus) {
     border-color: var(--hiq-input-invalid-border-color, var(--hiq-color-danger, hsl(352, 95%, 61%)));
   }
+
   &:required:valid:not(:focus) {
     border-color: var(--hiq-input-valid-border-color, var(--hiq-color-success, hsl(158, 73%, 48%)));
   }
+
+  ${props => props.isValid && css`
+    border-color: var(--hiq-color-success);
+  `}
 `;
 
 
@@ -74,6 +84,15 @@ const StyledLabel = styled.label`
   font-weight: var(--hiq-label-font-weight,var(--hiq-font-weight-medium,500));
   margin: 0 0 var(--hiq-label-margin-bottom,.25rem);
   padding: 0;
+`;
+
+const StyledError = styled.small`
+  display: none;
+
+  ${props => !props.isValid && css`
+    color: var(--hiq-color-danger);
+    display: block;
+  `}
 `;
 
 class Input extends Component {
@@ -135,8 +154,7 @@ class Input extends Component {
   }
 
   setValidity (input) {
-    // const input = this.x;
-
+    console.log(input);
     if (this.state.focused &&
         (!input.checkValidity() ||
           (input.required && !input.value.trim().length)
@@ -182,7 +200,9 @@ class Input extends Component {
       onChange={this.handleChange}
       onFocus={this.handleFocus}
       onBlur={this.handleBlur} >
-      <StyledLabel htmlFor={id}>{label}</StyledLabel>
+      <StyledLabel htmlFor={id}>
+        {label}
+      </StyledLabel>
       <StyledInput
         isValid={this.state.valid}
         aria-required={required}
@@ -209,12 +229,11 @@ class Input extends Component {
         ref={(input) => { this.input = input; }}
       />
       {title &&
-        <small
-          className={`is-alert-message ${this.state.valid ? 'is-success' : 'is-error'}`}
+        <StyledError
           role='alert'
           aria-atomic='true'>
           {title}
-        </small>
+        </StyledError>
       }
     </div>;
   }
